903b8c96f72bad0b9de11ed7123fee38c2971951
Austin <austin404ellis@gmail.com>
2023-10-27 07:02:37 -0500

    stylized navbar, portrait, and portrait/routes container

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Home.css'
 
 function Home() {
     return (
@@ -6,13 +7,13 @@ function Home() {
             {/* <div className='intro'>
             Hello my name is...
             </div> */}
-            <div className='nameTitle'>
+            <div id='nameTitle'>
                 Austin Ellis
             </div>
-            <div className='jobTitle'>
+            <div id='jobTitle'>
                 Software Engineer
             </div>
-            <div className='jobDescription'>
+            <div id='jobDescription'>
                 Committed, focused and motivated software engineer always on the lookout for more knowledge and unique challenges.
             </div>
         </div>
